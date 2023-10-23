9ab942550466c96ca6a25a56f90b76c9aec748f0
sumi-0011 <selina2000@naver.com>
2023-10-23 22:25:23 +0900

    chore: emotion css jsx error fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  compiler: {
+    reactRemoveProperties: isProd && {
+      properties: ["^data-testid"],
+    },
+    removeConsole: isProd && {
+      exclude: ["error", "warn"],
+    },
+  },
+};
+
+module.exports = nextConfig;
