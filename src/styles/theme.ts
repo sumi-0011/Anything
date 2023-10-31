3e94cacc94245c315cccf7a569bad3fe994bda26
sumi-0011 <selina2000@naver.com>
2023-10-31 23:06:33 +0900

    feat: common theme color 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,8 +12,23 @@ const zIndex = {
   below: (n: number) => n - 1,
 } as const;
 
+const colors = {
+  rose: "#F7CAC9	",
+  serenity: "#92A8D1",
+  white: "#FFFFFF",
+  black: "#454545",
+  pantone: {
+    100: "#92A8D1",
+    200: "#c5b9cd",
+    300: "#f7cac9",
+    400: "#eaa8b3",
+    500: "#dd869c",
+  },
+};
+
 const theme = {
   zIndex,
+  colors,
 } as const;
 
 export default theme;
