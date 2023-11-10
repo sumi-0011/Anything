a112110b32a8c78071a6e6e02c03f745f7a4a528
sumi-0011 <selina2000@naver.com>
2023-11-10 18:56:19 +0900

    style: panel item style storybook에 추가

--- src/components/Popover/index.stories.tsx
+++ src/components/Popover/index.stories.tsx
@@ -1,3 +1,4 @@
+import { css, Theme } from "@emotion/react";
 import { type Meta } from "@storybook/react";
 
 import Popover from "./index";
@@ -14,11 +15,25 @@ export function Default() {
     <Popover>
       <Popover.Button>Popover</Popover.Button>
       <Popover.Panel>
-        <div>Popover Panel</div>
+        <div css={liCss}>Item 1</div>
+        <div css={liCss}>Item 2</div>
+        <div css={liCss}>Item 3</div>
+        <div css={liCss}>Item 4</div>
       </Popover.Panel>
     </Popover>
   );
 }
+
+const liCss = (theme: Theme) => css`
+  font-size: 12px;
+  padding: 4px 8px;
+  background-color: ${theme.colors.text.default};
+  color: ${theme.colors.bg.default};
+
+  & + & {
+    margin-top: 8px;
+  }
+`;
 
 export function Overlay() {
   return (
