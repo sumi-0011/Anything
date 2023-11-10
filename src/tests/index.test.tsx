8c3490a04e8f482d1aefd85de888fa4b5de32afb
byun sumi <sumi@adenasoft.com>
2023-11-10 11:37:31 +0900

    chore: test eslint 적용

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -1,13 +1,11 @@
 import { describe, expect, it, test } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
 
 const sum = (a: number, b: number): number => a + b;
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
-
-import { render, screen } from "@testing-library/react";
-import React from "react";
 
 const App = () => {
   return <div>Hello world! I am using React</div>;
