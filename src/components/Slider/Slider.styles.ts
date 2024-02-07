5678dd5ada7b67427606f372c6133b86950c9c9f
sumi-0011 <selina2000@naver.com>
2024-02-07 14:28:33 +0900

    refactor: 이전 슬라이더 제거

--- src/components/Slider/Slider.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const ArrowContainer = styled.div`
-  position: absolute;
-  margin: auto;
-  height: fit-content;
-  top: 0;
-  bottom: 0;
-`;
--- src/pages/slider.page.tsx
+++ src/pages/slider.page.tsx
@@ -1,9 +1,6 @@
 import styled from "@emotion/styled";
 
-import FullSlider from "@/components/Slider/v2/FullSlider";
-// import FullSlider from "@/components/Slider/FullSlider";
-// import SingleSlider from "@/components/Slider/SingleSlider";
-import SingleSlider2 from "@/components/Slider/v2/SingleSlider";
+import MobileSlider from "@/components/Slider/MobileSlider";
 import { theme } from "@/styles/theme";
 
 function SliderPage() {
@@ -13,44 +10,10 @@ function SliderPage() {
 
   return (
     <Container>
-      {/* <section>
-        <Heading>single slider (blur false, stepper false)</Heading>
-        <SingleSlider sliderSize={300} slidesToShow={2} isBlur>
-          <SingleSlider.Inner>{InnerData}</SingleSlider.Inner>
-          <SingleSlider.PrevArrow />
-          <SingleSlider.NextArrow />
-        </SingleSlider>
-      </section>
       <section>
         <Heading>full slider (blur false, stepper false)</Heading>
-        <FullSlider sliderSize={300} slidesToShow={2} isBlur>
-          <FullSlider.Inner>{InnerData}</FullSlider.Inner>
-          <FullSlider.PrevArrow />
-          <FullSlider.NextArrow />
-        </FullSlider>
-      </section> */}
-      <section>
-        <Heading>full slider (blur false, stepper false)</Heading>
-        <SingleSlider2 sliderSize={300} slidesToShow={2}>
-          {InnerData}
-        </SingleSlider2>
-      </section>{" "}
-      <section>
-        <Heading>full slider (blur false, stepper fal se)</Heading>
-        <FullSlider slidesToShow={2}>{InnerData}</FullSlider>
+        <MobileSlider>{InnerData}</MobileSlider>
       </section>
-      {/* <section>
-        <Heading>single slider (blur true, stepper false)</Heading>
-        <SingleSlider sliderSize={300} slidesToShow={2}>
-          {InnerData}
-        </SingleSlider>
-      </section>
-      <section>
-        <Heading>single slider (stepper true)</Heading>
-        <SingleSlider sliderSize={300} slidesToShow={2}>
-          {InnerData}
-        </SingleSlider>
-      </section> */}
     </Container>
   );
 }
