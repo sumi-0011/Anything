d1486379e8ae6fc8a7cbaf8a66ba9f1d7467c146
sumi-0011 <selina2000@naver.com>
2024-02-07 14:24:56 +0900

    feat: slider full

--- src/pages/slider.page.tsx
+++ src/pages/slider.page.tsx
@@ -1,20 +1,43 @@
 import styled from "@emotion/styled";
 
-import SingleSlider from "@/components/Slider/SingleSlider";
+import FullSlider from "@/components/Slider/v2/FullSlider";
+// import FullSlider from "@/components/Slider/FullSlider";
+// import SingleSlider from "@/components/Slider/SingleSlider";
+import SingleSlider2 from "@/components/Slider/v2/SingleSlider";
 import { theme } from "@/styles/theme";
 
 function SliderPage() {
-  const InnerData = [1, 2, 3, 4, 5].map((item) => <Box key={item}>{item}</Box>);
+  const InnerData = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
+    <Box key={item}>{item}</Box>
+  ));
 
   return (
     <Container>
-      <section>
+      {/* <section>
         <Heading>single slider (blur false, stepper false)</Heading>
         <SingleSlider sliderSize={300} slidesToShow={2} isBlur>
           <SingleSlider.Inner>{InnerData}</SingleSlider.Inner>
           <SingleSlider.PrevArrow />
           <SingleSlider.NextArrow />
         </SingleSlider>
+      </section>
+      <section>
+        <Heading>full slider (blur false, stepper false)</Heading>
+        <FullSlider sliderSize={300} slidesToShow={2} isBlur>
+          <FullSlider.Inner>{InnerData}</FullSlider.Inner>
+          <FullSlider.PrevArrow />
+          <FullSlider.NextArrow />
+        </FullSlider>
+      </section> */}
+      <section>
+        <Heading>full slider (blur false, stepper false)</Heading>
+        <SingleSlider2 sliderSize={300} slidesToShow={2}>
+          {InnerData}
+        </SingleSlider2>
+      </section>{" "}
+      <section>
+        <Heading>full slider (blur false, stepper fal se)</Heading>
+        <FullSlider slidesToShow={2}>{InnerData}</FullSlider>
       </section>
       {/* <section>
         <Heading>single slider (blur true, stepper false)</Heading>
